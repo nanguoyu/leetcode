891adda4b860eb616ff71d4b42acd7b1a4116ae2
nanguoyu <1130658575@qq.com>
2019-03-18 11:27:15 +0800

    add No.40 Question.

--- main.cpp
+++ main.cpp
@@ -9,62 +9,47 @@
 
 using namespace std;
 
-/**
- * Return an array of arrays of size *returnSize.
- * Note: The returned array must be malloced, assume caller calls free().
- */
-
 int comp(const void *a, const void *b)
 {
     return *(int *) a - *(int *) b;
 }
 
-int threeSumClosest(int *nums, int numsSize, int target)
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+void swap(int *a, int *b)
 {
+    int c = *a;
+    *a = *b;
+    *b = c;
+}
 
-#define  nullptr 0
-    if (numsSize < 3) {
-        return nullptr;
+int firstMissingPositive(int* nums, int numsSize) {
+    int i = 0;
+    while( i < numsSize) {
+        if(nums[i]>0&& nums[i]<numsSize && nums[i]!=i+1 && nums[nums[i]-1]!=nums[i]){
+            swap(nums+nums[i]-1,nums+i);
+        };
+        if(nums[i]==i+1 || nums[i]<=0 || nums[i]>=numsSize ||nums[nums[i]-1]==nums[i]){++i;}
     }
-    qsort(nums, (size_t) numsSize, sizeof(int), comp);
-    int ClosetLength = INT_MAX;
-    int ClostNumber = target;
 
-    for (int i = 0; i < numsSize - 2; ++i) {
-        int j = i + 1;
-        int k = numsSize - 1;
-        while (j < k) {
-            int length = target - nums[j] - nums[k] - nums[i];
 
-            if (length == 0) {
-                return target;
-            } else if (length < 0) {
-                if (-length < ClosetLength) {
-                    ClosetLength = -length;
-                    ClostNumber = nums[j] + nums[k] + nums[i];
-                }
-
-                while (k > 0 && nums[k] == nums[k - 1]) k--;
-                k--;
-            } else {
-                if (length < ClosetLength) {
-                    ClosetLength = length;
-                    ClostNumber = nums[j] + nums[k] + nums[i];
-                }
-
-                while (j < numsSize - 1 && nums[j] == nums[j + 1]) j++;
-                j++;
-            }
-
-        }
+    int j=0;
+    for (; j < numsSize; ++j) {
+        if (j!=nums[j]-1) break;
     }
-    return ClostNumber;
+    return j+1;
 }
 
 int main()
 {
-    int a[4] = {1, 1, 1, 0};
-    int b = -100;
-    cout<<threeSumClosest(a,4,-100)<<endl;
+    int numbers[]={1,1}; int numsSize=3;
+    int *nums = numbers;
+    int res = firstMissingPositive(nums,numsSize);
+    printf("%d\n",res);
+
     return 0;
 }
+
